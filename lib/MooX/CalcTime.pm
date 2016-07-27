dd6bdbd280c8da395b58877925641f86de67dbf8
Yan Xueqing <yanxueqing001@berrygenomics.com>
2016-07-27 15:55:39 +0800

    Added moox-calctime module

--- lib/MooX/CalcTime.pm
+++ lib/MooX/CalcTime.pm
@@ -1,17 +1,35 @@
 package MooX::CalcTime;
 use Modern::Perl;
+use Moo;
+
+with 'MooX::CalcTime::Role';
 
 # VERSION
-# ABSTRACT: MooX::CalcTime
+# ABSTRACT: This is a instantial object of MooX::CalcTime::Role
 
 =head1 SYNOPSIS
 
   use MooX::CalcTime;
-  ...
+  my $t = MooX::CalcTime;
+  ......
+  ......
+
+  # return second passed, such as 30
+  $t->get_run_second;
+
+  # return a string such as 'Running time: 3 days 2 minutes 1 hours 10 minutes 5 second';
+  $t->get_runtime_format; # return a variable: '3 days 2 hours 1 minute'
+
+  # print return value of C<get_runtime_format> function
+  $t->print_runtime;
 
 =head1 DESCRIPTION
 
-Blablabla
+This module is a instantial object of MooX::CalcTime::Role,
+so that it can be used in a script.
+
+If you want to see more detailed information,
+please see L<MooX::CalcTime::Role>.
 
 =cut
 
